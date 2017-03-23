eee98d5681323403ac7627026d71d8f5c959f95b
Sharat Maharjan <sharatmaharjan@gmail.com>
2017-03-23 13:30:13 +0545

    first controller and greeting page

---
+++ CVStoreMongodb/src/main/java/com/cvstore/controller/CVController.java
@@ -0,0 +1,14 @@
+package com.cvstore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class CVController {
+
+	@RequestMapping(value="/",method=RequestMethod.GET)
+	public String welcomePage(){
+		return "greeting";
+	}
+}
